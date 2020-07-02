57a7c23da39b86bb62cc07d228253164663baeb3
Abel Kisebela <kisebela.a@gmail.com>
2020-07-02 20:17:15 +0200

    Ajout des composants de base

--- SiwaApp/src/app/app.module.ts
+++ SiwaApp/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { GraphiqueComponent } from './graphique/graphique.component';
+import { RouterModule, Router } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ProduitsComponent,
+    AdministrationComponent,
+    GraphiqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'produits', component:ProduitsComponent },
+      { path: 'administration', component:AdministrationComponent },
+      { path: 'graphique', component:GraphiqueComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
